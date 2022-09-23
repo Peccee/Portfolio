0432a24098dc497d0767c646fd34bac15b3cc0bd
Peccee <111501168+Peccee@users.noreply.github.com>
2022-09-23 18:54:46 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pjovanovic_digitel' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'pjovanovic_digitelUser' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'I0t;b8RAK7XT' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
